14ce3aa587b3db8d50aa1b7c52b5c8bdcc71c8d3
Krist Wongsuphasawat <krist.wongz@gmail.com>
2016-02-29 00:16:27 -0800

    update npm scripts

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -10,31 +10,5 @@ module.exports = {
       'uglify:dist',
       'cssmin:dist'
     ]
-  },
-
-  'publish:patch': {
-    description: 'Bundle code, bump version by 0.0.1 and publish to npm and bower',
-    tasks: [
-      'build',
-      'bump:patch',
-      'shell:publish'
-    ]
-  },
-  'publish:minor': {
-    description: 'Bundle code, bump version by 0.1 and publish to npm and bower',
-    tasks: [
-      'build',
-      'bump:minor',
-      'shell:publish'
-    ]
-  },
-  'publish:major': {
-    description: 'Bundle code, bump version by 1 and publish to npm and bower',
-    tasks: [
-      'build',
-      'bump:major',
-      'shell:publish'
-    ]
-  },
-  'publish': ['publish:patch']
+  }
 };
